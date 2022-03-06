139ba6fd83ab4a173195604d674cee9b498c5bae
Maaax02 <massiown@gmail.com>
2022-03-06 20:02:07 +0100

    add style, error with bootstrap icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
     <body>
         <div class="containter">
-            @foreach($movies as $key)
-                <div class="card">
-                    <h1>{{$key['title']}}</h1>
-                    <p>{{$key['original_title']}}</p>
-                </div>
-            @endforeach
+            <div class="container">
+                @foreach($movies as $key)
+                    <div class="card">
+                        <div>
+                            <h2>{{$key['title']}}</h2>
+                            <p>Nationality: '{{$key['nationality']}}'</p>
+                            <p>{{$key['date']}}</p>
+                            <div class="vote">
+                                @for ($i = 0; $i < 5; $i++)
+                                {{-- @if($i < floor($key['vote']) / 2)
+                                <i class="bi bi-star-fill"></i>
+                                @else
+                                
+                                @endif --}}
+                                    <i class="bi bi-star-fill"></i>
+                                @endfor
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </body>
 </html>
